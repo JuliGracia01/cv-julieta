fa16f2a87faee7f1e4bcc03e16675645b8655544
juli <juligracia2016@gmail.com>
2023-10-22 11:24:54 -0300

    agregué un archvi js y un sonido en el botón del formulario

---
+++ js/audio.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+/*SONIDO BOTÓN ENVIAR*/
+function playSound() {
+	var audio = document.getElementById("myAudio");
+	audio.play();
+} /*bueno funcionar el sonido funciona pero no entiendo por qué me marca un error*/
